32e071bf3df1e31508784098bca38d8030d57b73
AFakeman <anton_suslov@me.com>
2018-02-15 20:26:04 +0300

    Exponential backoff for expecting connections, removed debug print

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,15 +1,27 @@
 import pika
 import psycopg2
+import time
 
-while True:
-    try:
-        pika_conn = pika.BlockingConnection( 
-          pika.URLParameters("amqp://guest:guest@queue:5672") 
-        )
-    finally:
-        break
+timeout = 0.01
+try:
+    pika_conn = pika.BlockingConnection( 
+      pika.URLParameters("amqp://guest:guest@queue:5672") 
+    )
+    postgres_conn = psycopg2.connect("dbname=queue user=consumer host=database")
+except:
+    success = False
+    while not success:
+        try:
+            time.sleep(timeout)
+            timeout *= 2
+            pika_conn = pika.BlockingConnection( 
+              pika.URLParameters("amqp://guest:guest@queue:5672") 
+            )
+            postgres_conn = psycopg2.connect("dbname=queue user=consumer host=database")
+            success = True
+        except:
+            success = False
 
-postgres_conn = psycopg2.connect("dbname=queue user=consumer host=database")
 
 channel = pika_conn.channel()
 channel.queue_declare(queue='hello')
@@ -17,7 +29,6 @@ channel.queue_declare(queue='hello')
 def callback(ch, method, properties, body):
     recieved = (body.decode())
     cursor = postgres_conn.cursor()
-    print(recieved)
     cursor.execute("INSERT INTO queue VALUES (DEFAULT, %s)", (recieved,))
     postgres_conn.commit()
     cursor.close()
@@ -26,3 +37,6 @@ channel.basic_consume(callback,
             queue='hello',
             no_ack=True)
 channel.start_consuming()
+
+postgress_conn.close()
+pika_conn.close()
